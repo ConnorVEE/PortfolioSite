f88d07082b949f8395f0fbae5713fa43e6959cda
Connor VanTress <11cvantress@gmail.com>
2026-08-13 12:44:59 -0400

    Add hero section styling and spacing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,50 @@
+import Container from "@/components/ui/Container";
+import Hero from "@/components/hero/Hero";
+
 export default function Home() {
   return (
     <main>
-      <h1>Connor VanTress Portfolio</h1>
+      <section>
+        <Container>
+          <Hero />
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <h2>About Me</h2>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <h2>Projects</h2>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <h2>Skills</h2>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <h2>How I Build</h2>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <h2>Experience</h2>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <h2>Contact</h2>
+        </Container>
+      </section>
     </main>
   );
 }
