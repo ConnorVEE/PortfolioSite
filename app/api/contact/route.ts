9367defc6eb670239a88f6d13413eeb959a526c8
Connor V <11cvantress@gmail.com>
2026-08-29 12:50:55 -0400

    Add infrastructure to support Resend email API and its use. Spam prevention and error handling to come next

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,11 +1,32 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function POST(request: Request) {
     const { name, email, message } = await request.json();
 
-    console.log(name);
-    console.log(email);
-    console.log(message);
+    const { data, error } = await resend.emails.send({
+        from: "Portfolio <onboarding@resend.dev>",  // This email will change once we aquire a real domain for the website
+        to: ["11cvantress@gmail.com"],
+        subject: `Portfolio Contact: ${name}`,
+        html: `
+            <h2>New portfolio contact</h2>
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Message:</strong></p>
+            <p>${message}</p>
+        `,
+    });
+
+    if (error) {
+        return Response.json(
+            { error },
+            { status: 500 }
+        );
+    }
 
     return Response.json({
-        message: "Contact form received!"
+        success: true,
+        data,
     });
 }
